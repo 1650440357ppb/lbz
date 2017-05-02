1fda101b48a0c1009c8debdae5ce019c5eaa5a71
谢兆邦 <547968365@qq.com>
2017-05-02 17:11:59 +0800

    官网新版

--- js/download.js
+++
@@ -1,9 +0,0 @@
-window.onload=function(){
-    //返回顶部
-    document.documentElement.scrollTop = 1;
-    var obj = document.documentElement.scrollTop?document.documentElement:document.body;
-    var returnTop=getClass("returnTop")[0];
-    returnTop.onclick=function(){
-        animate(obj,{scrollTop:0});
-    }
-}
